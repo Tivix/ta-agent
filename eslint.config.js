19f9183ba6cae5a0e65bb4fe465a549590aadc46
Konrad Jażownik <konrad.jazownik@kellton.com>
2025-02-19 12:31:31 +0100

    packae + eslint update

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      prettier,
+    },
+    rules: {
+      ...ts.configs['recommended'].rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-console': 'warn',
+    },
+  },
+  prettierConfig,
+];
